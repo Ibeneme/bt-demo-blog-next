398e9097a6dc3377d9fa9500316e25f89dd946b3
ibeneme <ikennaibenemee@gmail.com>
2026-06-05 19:17:00 +0100

    updates

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,4 +1,4 @@
-export default function Layout({ children }) {
+export default function Layout({ children }: any) {
   return (
     <div className="min-h-screen bg-white text-gray-900">
       <main className="mx-auto">{children}</main>
